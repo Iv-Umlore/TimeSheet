644162c88eda8b46623494f826735c442e58b038
ivan.kiselev <ivan.kiselev@stm-labs.ru>
2022-03-29 13:13:26 +0300

    Осознание логики, коммит до начала превращения консоли в утилиту

--- TimeSheet/Program.cs
+++ TimeSheet/Program.cs
@@ -8,26 +8,39 @@ namespace TimeSheet
 {
     class Program
     {
-        const string TfsUrl = "https://tfs.example.com/";
-        const string UserName = "Пушкин Александр Сергеевич";
+        //const string TfsUrl = "http://ikiselev:8080/tfs";
+        const string TfsUrl = "https://tfs.mtsit.com/STS";
+        const string UserName = "Горятнин Павел";
 
         static void Main(string[] args)
         {
-            GetCompletedWork();
+            var completedWorkToday = GetCompletedWork();
+            if (completedWorkToday < 7.0)
+            {
+                MakeNotification(completedWorkToday);
+            }
+
+
         }
 
-        static void GetCompletedWork()
+        /// <summary>
+        /// Вернуть число часов указанных за этот день
+        /// </summary>
+        [Obsolete]
+        static double GetCompletedWork()
         {
-
-            var startWeek = DateTime.Now.StartOfWeek(DayOfWeek.Monday);
-            var endWeek = startWeek.AddDays(7).AddSeconds(-1);
+            var startWeek = new DateTime(year: 2022, month: 3, day: 17);// DateTime.Now.StartOfWeek(DayOfWeek.Monday);
+            var endWeek = new DateTime(year: 2022, month: 3, day: 17, hour: 23, minute:59, second:59);// 
+            // var endWeek = startWeek.AddDays(7).AddSeconds(-1);
 
             using (var client = new TfsTeamProjectCollection(new Uri(TfsUrl))) 
             {
                 var queryStr = GetChangedItemQuery(startWeek, endWeek);
+                //var queryStr = GetItemChangedByMe(startWeek, endWeek);
                 var query = new Query(client.GetService<WorkItemStore>(), queryStr, null, false);
 
                 var request = query.BeginQuery();
+
                 var workItems = query.EndQuery(request)
                     .AsParallel()
                     .Cast<WorkItem>()
@@ -42,13 +55,29 @@ namespace TimeSheet
                     Console.WriteLine($"{item.WI}: {item.Hours}");
 
                 Console.WriteLine($"total: {workItems.Sum(x => x.Hours)}");
+                return workItems.Sum(x => x.Hours);
+
+
             }
         }
 
-        static string GetChangedItemQuery(DateTime dateFrom, DateTime dateTo) => $"SELECT [System.Id], [System.WorkItemType]" +
-            $" FROM WorkItems WHERE [System.ChangedDate] >= '{dateFrom}' AND [System.ChangedDate] <= '{dateTo}'" +
+        static string GetItemChangedByMe(DateTime Today) =>
+            $"SELECT [System.Id], [System.WorkItemType] " +
+            $" FROM WorkItems WHERE [System.ChangedDate] >= '{Today.Date}' AND [System.ChangedDate] <= '{Today.Date.AddDays(1).AddSeconds(-1)}'" +
             $" AND ([System.WorkItemType] = 'Bug' OR [System.WorkItemType] = 'Issue' OR [System.WorkItemType] Contains 'Task')" +
             $" AND ([System.ChangedBy] EVER @Me)";
+
+        static string GetChangedItemQuery(DateTime dateFrom, DateTime dateTo) => $"SELECT * " + //
+            $" FROM WorkItems WHERE [System.Id] = 2691945 AND [System.ChangedDate] >= '{dateFrom}' AND [System.ChangedDate] <= '{dateTo}'";/*  */
+            /*$"[System.ChangedDate] >= '{dateFrom}' AND [System.ChangedDate] <= '{dateTo}'" +
+            $" AND ([System.WorkItemType] = 'Bug' OR [System.WorkItemType] = 'Issue' OR [System.WorkItemType] Contains 'Task')";// +
+            //$" AND ([System.ChangedBy] EVER @Me)";*/
+
+        static void MakeNotification(double completeWorkNow)
+        {
+            // Уведомление о том, что вы плохо сделали
+        }
+
     }
 
     static class DateTimeExtensions
